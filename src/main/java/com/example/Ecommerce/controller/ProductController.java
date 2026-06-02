72a250f1b4ca3f7aa2ee7ffdab0b1ecc66ac4ef9
Gufran Ahmed <gufranahmed0921@gmail.com>
2026-06-02 14:34:55 +0530

    Create api for getting product by keyword

--- src/main/java/com/example/Ecommerce/controller/ProductController.java
+++ src/main/java/com/example/Ecommerce/controller/ProductController.java
@@ -31,7 +31,12 @@ public class ProductController {
 
     @GetMapping("public/categories/{categoryId}/products")
     public ResponseEntity<ProductResponse> getProductByCategory(@PathVariable Long categoryId){
-        ProductResponse productResponse=productService.searchByCategory(categoryId);
+        ProductResponse productResponse=productService.searchByCategory(categoryId); 
         return new ResponseEntity<>(productResponse,HttpStatus.OK);
+    }
+    @GetMapping("public/products/keyword/{keyword}")
+    public ResponseEntity<ProductResponse> getProductByKeyword(@PathVariable String keyword){
+        ProductResponse productResponse=productService.searchProductByKeyword(keyword);
+        return new ResponseEntity<>(productResponse,HttpStatus.FOUND);
     }
 }
