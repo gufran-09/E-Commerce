d7fbee256e830a65f36cf933871f320c4605a4c8
Gufran Ahmed <gufranahmed0921@gmail.com>
2026-06-29 19:31:38 +0530

    Created custom UserDetailsService interface

--- src/main/java/com/example/Ecommerce/security/services/UserDetailsServiceImplementation.java
+++ src/main/java/com/example/Ecommerce/security/services/UserDetailsServiceImplementation.java
@@ -1,4 +1,27 @@
 package com.example.Ecommerce.security.services;
 
-public class UserDetailsServiceImplementation {
+import com.example.Ecommerce.model.User;
+import com.example.Ecommerce.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImplementation implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user=userRepository.findByUserName(username)
+                .orElseThrow(()->
+                        new UsernameNotFoundException("User not found with username: "+username));
+        return UserDetailsImplementation.build((user));
+    }
+
 }
